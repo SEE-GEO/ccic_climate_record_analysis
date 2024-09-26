e8223caff3806d6e75004c2f15ed6d60bebb20b0
Simon Pfreundschuh <simon.pfreundschuh@colostate.edu>
2024-09-26 14:59:43 +0200

    Minor refactoring.

--- scripts/cloudsat/download_cloudsat.py
+++ scripts/cloudsat/download_cloudsat.py
@@ -1,17 +1,34 @@
-from  pysftp import Connection, SSHException
+"""
+This script downloads CloudSat/Calipso data used for the analysis of the CCIC
+climate record.
+"""
+
+from pysftp import Connection, SSHException
 import os
 from datetime import datetime, timedelta
 from getpass import getpass
 
+
 def download_files_sftp(host, user, remote_paths, local_path, key=None, pw=None):
     """
-    remote_paths: list of remote directories
+    Downloads a list of files from an SFTP server.
+
+    Args:
+        host: URL of the sftp server
+        user: The sftp user name
+        remote_paths: List of the paths to download.
+        local_path: The local path to which to download the files.
+        key: Key to use for authentication.
+        pw: Password to use for autentication.
+
+    Return:
+        A list of the paths that were downloaded.
     """
     # Connect to the SFTP server
     try:
         with Connection(host, username=user, private_key=key, password=pw) as sftp:
             ok_paths = []
-            for i,remote_path in enumerate(remote_paths):
+            for i, remote_path in enumerate(remote_paths):
                 try:
                     # Change to the remote directory
                     sftp.chdir(remote_path)
@@ -20,7 +37,7 @@ def download_files_sftp(host, user, remote_paths, local_path, key=None, pw=None)
                     ok_paths.append(i)
                 except:
                     file_list = []
-                #assert len(file_list) > 0, f'No files found at remote path {remote_path}'
+                # assert len(file_list) > 0, f'No files found at remote path {remote_path}'
 
                 for file_name in file_list:
 
@@ -30,19 +47,34 @@ def download_files_sftp(host, user, remote_paths, local_path, key=None, pw=None)
                     # Download file
                     sftp.get(remote_file_path, local_file_path)
 
-                    #print(f'Downloaded: {file_name}')
-                print(f"Downloaded files for path {i+1}/{len(remote_paths)}: {remote_path}")
+                    # print(f'Downloaded: {file_name}')
+                print(
+                    f"Downloaded files for path {i+1}/{len(remote_paths)}: {remote_path}"
+                )
     except SSHException as e:
         ok_paths = []
         print("Fail to connect to remote path", e)
     return ok_paths
 
+
 def download_dardar(
-        credentials :   dict,
-        year        :   int, 
-        ordinal_days:   list[int], 
-        destination :   str,
-    ):
+    credentials: dict,
+    year: int,
+    ordinal_days: list[int],
+    destination: str,
+):
+    """
+    Downloads DARDAR files for a list of days.
+
+    Args:
+        credentials: A dictionary containign the credentials for the ICARE server.
+        year: The year from which to download the files.
+        ordinal_days: The julian days for which to download the files.
+        destination: The folder to which to write the files.
+
+    Return:
+        A list of the downloaded files.
+    """
     remote_paths = []
     for day in ordinal_days:
         # Create datetime from year and ordinal day
@@ -51,84 +83,128 @@ def download_dardar(
         date = date.strftime("%Y_%m_%d")
 
         remote_path = os.path.join(
-            '/SPACEBORNE/CLOUDSAT/DARDAR-CLOUD.v3.10/', str(year), date
+            "/SPACEBORNE/CLOUDSAT/DARDAR-CLOUD.v3.10/", str(year), date
         )
         remote_paths.append(remote_path)
 
     ok = download_files_sftp(
-        credentials['host'], 
-        credentials['user'],  
-        remote_paths, 
+        credentials["host"],
+        credentials["user"],
+        remote_paths,
         destination,
-        pw=credentials['password'],
+        pw=credentials["password"],
     )
     return ok
 
+
 def download_2cice(
-        credentials :   dict,
-        year        :   int, 
-        ordinal_days:   list[int], 
-        destination :   str,
-    ):
+    credentials: dict,
+    year: int,
+    ordinal_days: list[int],
+    destination: str,
+):
+    """
+    Download 2C-ICE files for a list of Julian days for a given year from
+    the CloudSat DPC.
+
+    Args:
+        credentials: A dictionary containing the user credentials for
+            the CloudSat DPC.
+        year: The year
+        ordinal_days: The Julian days for which to download the files.
+        destination: The local path to which to download the files.
+
+    Return:
+        A list of the downloaded files.
+    """
     remote_paths = []
     for day in ordinal_days:
         remote_path = f"/Data/2C-ICE.P1_R05/{year:04}/{day:03}"
         remote_paths.append(remote_path)
 
     ok = download_files_sftp(
-        credentials['host'], 
-        credentials['user'],  
-        remote_paths, 
+        credentials["host"],
+        credentials["user"],
+        remote_paths,
         destination,
-        key=credentials['password'],
+        key=credentials["password"],
     )
     return ok
 
+
 def download_cldclass(
-        credentials :   dict,
-        year        :   int, 
-        ordinal_days:   list[int], 
-        destination :   str,
-    ):
+    credentials: dict,
+    year: int,
+    ordinal_days: list[int],
+    destination: str,
+):
+    """
+    Download 2B-CLDCLASS files for a list of Julian days for a given year from.
+
+    Args:
+        credentials: A dictionary containing the user credential
+        year: The year
+        ordinal_days: The Julian days for which to download the files.
+        destination: The local path to which to download the files.
+
+    Return:
+        A list of the downloaded files.
+
+    """
     remote_paths = []
     for day in ordinal_days:
         remote_path = f"/Data/2B-CLDCLASS.P1_R05/{year:04}/{day:03}"
         remote_paths.append(remote_path)
 
     ok = download_files_sftp(
-        credentials['host'], 
-        credentials['user'], 
-        remote_paths, 
+        credentials["host"],
+        credentials["user"],
+        remote_paths,
         destination,
-        key=credentials['password'],
+        key=credentials["password"],
     )
     return ok
 
 
-def download_data(dataset : str, cred  :  dict, date : datetime, basedir='./'):
+def download_data(dataset: str, cred: dict, date: datetime, basedir="./"):
+    """
+    Downloads DARDAR, 2C-ICE, or 2B-CLDCLASS data.
 
+    Args:
+        dataset: The name of the dataset to download. Should be one of
+            'dardar', '2cice', 'cldclass'.
+        cred: The user credential to use for authentication.
+        date: A date object specifying the date for which to download the data.
+        basedir: The data folder to which to download the data.
+
+    Return:
+        A list of the downloaded files.
+
+    """
     output_dirs = {
-        'dardar'    :   'DARDAR/dardar3.1',
-        '2cice'     :   '2C-ICE',
-        'cldclass'  :   '2B-CLDCLASS',
+        "dardar": "DARDAR/dardar3.1",
+        "2cice": "2C-ICE",
+        "cldclass": "2B-CLDCLASS",
     }
-    
-    assert dataset in output_dirs.keys(), f'{dataset}, is not a valid dataset'
+
+    assert dataset in output_dirs.keys(), f"{dataset}, is not a valid dataset"
 
     year = date.year
     mon = date.month
     day = date.day
     ordinal_day = date.timetuple().tm_yday
-    
+
     # path to store downloaded data
-    local_path = os.path.join(basedir, output_dirs[dataset], str(year), str(mon).zfill(2), str(day).zfill(2)) 
+    local_path = os.path.join(
+        basedir, output_dirs[dataset], str(year), str(mon).zfill(2), str(day).zfill(2)
+    )
     # Create local directory if it doesn't exist
     os.makedirs(local_path, exist_ok=True)
-    
+
     # Download data
-    if dataset == 'dardar':
+    if dataset == "dardar":
         day_ok = download_dardar(cred, year, [ordinal_day], local_path)
-    elif dataset == '2cice':
+    elif dataset == "2cice":
         day_ok = download_2cice(cred, year, [ordinal_day], local_path)
     else:
         day_ok = download_cldclass(cred, year, [ordinal_day], local_path)
@@ -137,27 +213,26 @@ def download_data(dataset : str, cred  :  dict, date : datetime, basedir='./'):
     if len(day_ok) == 0:
         try:
             os.rmdir(local_path)
-            print(f'No data found for {start_date.date()}')
+            print(f"No data found for {start_date.date()}")
         except OSError as e:
-            print(f'Error: {local_path} : {e.strerror}')
-
+            print(f"Error: {local_path} : {e.strerror}")
 
 
 if __name__ == "__main__":
 
-    basedir = './'
+    basedir = "./"
 
     # SFTP server credentials for downloading DARDAR
     icare_cred = {
-        'host'      :   'sftp.icare.univ-lille.fr',
-        'user'      :   'hannahallborn',
-        'password'  :   getpass('Icare password: '),
+        "host": "sftp.icare.univ-lille.fr",
+        "user": "hannahallborn",
+        "password": getpass("Icare password: "),
     }
     # SFTP server credentials for downloading 2C-ICE and 2B-CLDCLASS
     cloudsat_cred = {
-        'host'      :   'www.cloudsat.cira.colostate.edu',
-        'user'      :   'hallbornATchalmers.se',
-        'password'  :   'PATH/TO/KEY',
+        "host": "www.cloudsat.cira.colostate.edu",
+        "user": "hallbornATchalmers.se",
+        "password": "PATH/TO/KEY",
     }
 
     start_date = datetime(2017, 1, 1)
@@ -166,17 +241,12 @@ if __name__ == "__main__":
     while start_date <= end_date:
 
         # Download DARDAR
-        download_data('dardar', icare_cred, start_date, basedir)
+        download_data("dardar", icare_cred, start_date, basedir)
 
         # Download 2C-ICE
-        download_data('2cice', cloudsat_cred, start_date, basedir)
-        
+        download_data("2cice", cloudsat_cred, start_date, basedir)
+
         # Download 2B-CLDCLASS
-        download_data('cldclass', cloudsat_cred, start_date, basedir)
+        download_data("cldclass", cloudsat_cred, start_date, basedir)
 
         start_date += timedelta(days=1)
-
-
-
-
-
--- scripts/cloudsat/preprocess_cloudsat.py
+++ scripts/cloudsat/preprocess_cloudsat.py
@@ -1,3 +1,10 @@
+"""
+This script combins DARDAR, 2C-ICE, and 2B-CLDCLASS data and extracts the 
+corresponding CCIC GridSat results.
+"""
+
+from typing import Any, Dict, List
+
 import numpy as np
 import xarray as xr
 import os
@@ -7,108 +14,141 @@ import multiprocessing
 
 import data_preprocessing as dp
 
-GRIDSAT_dt = 3 # 3h timesteps for Gridsat  
+GRIDSAT_dt = 3  # 3h timesteps for Gridsat
 
-def get_granules(year, month, cloudsat_paths):
+
+def get_granules(
+    year: int, month: int, cloudsat_paths: List[str]
+) -> Dict[str, Dict[str, Any]]:
+    """
+    Get available CloudSat granules for a given month.
+
+    Args:
+        year: The year
+        month: The mont
+        cloudsat_path: The paths containing the CloudSat data.
+
+    Return:
+        A dictionary mapping granule numbers to corresponding files
+        and time stamps.
+    """
     granules = {}
     for product in cloudsat_paths:
-        files = glob.glob(f'{cloudsat_paths[product]}/{year}/{month:02}/*/*')
+        files = glob.glob(f"{cloudsat_paths[product]}/{year}/{month:02}/*/*")
         for f in files:
             file = Path(f)
             filename = file.name
-            if filename.endswith('.hdf'):
-                tstamp = filename.split('_')[0]
-                id_ = filename.split('_')[1]
-            elif filename[0:6] == 'DARDAR':
-                tstamp = filename.split('_')[1]
-                id_ = filename.split('_')[2]
+            if filename.endswith(".hdf"):
+                tstamp = filename.split("_")[0]
+                id_ = filename.split("_")[1]
+            elif filename[0:6] == "DARDAR":
+                tstamp = filename.split("_")[1]
+                id_ = filename.split("_")[2]
             else:
                 id_ = 0
                 tstamp = 0
-                print('No match for ', filename)
+                print("No match for ", filename)
 
             if id_ not in granules.keys():
-                granules[id_] = {'files' : [file], 'tstamp' : tstamp}
+                granules[id_] = {"files": [file], "tstamp": tstamp}
             else:
-                granules[id_]['files'].append(file)
+                granules[id_]["files"].append(file)
     return granules
 
-def process_granule(id_, granules, paths):
+
+def process_granule(
+    id_: str, granules: Dict[str, Dict[str, Any]], paths: Dict[str, str]
+) -> str:
+    """
+    Process a granule.
+
+    Args:
+        id_: The granule identification string.
+        granules: The dictionary containing the files corresponding to each granule.
+        paths: A dictionary containing the paths to the output an ccic_datadir.
+
+    Return:
+        A message indicating the processing status.
+    """
     # no local path needed
     # get basename from the full path
-    output_dir = paths['output_dir']
-    ccic_datadir = paths['ccic_datadir']
-    
-    if len(granules[id_]['files']) == 3:    
-        
-        tstamp = granules[id_]['tstamp']
+    output_dir = paths["output_dir"]
+    ccic_datadir = paths["ccic_datadir"]
+
+    if len(granules[id_]["files"]) == 3:
+
+        tstamp = granules[id_]["tstamp"]
         # Load CloudSat data
         allfiles_ok = 0
-        for f in granules[id_]['files']:
-            filename = f.name 
-            if filename.startswith('DARDAR'):
+        for f in granules[id_]["files"]:
+            filename = f.name
+            if filename.startswith("DARDAR"):
                 dardar = dp.load_dardar(f)
                 allfiles_ok += 1
-            elif filename.startswith(f'{tstamp}_{id_}_CS_2C-ICE'):
+            elif filename.startswith(f"{tstamp}_{id_}_CS_2C-ICE"):
                 ice2c = dp.load_2cice(f)
                 allfiles_ok += 1
-            elif filename.startswith(f'{tstamp}_{id_}_CS_2B-CLDCLASS'):
+            elif filename.startswith(f"{tstamp}_{id_}_CS_2B-CLDCLASS"):
                 cldclass = dp.load_cldclass(f)
                 allfiles_ok += 1
             else:
-                print(f'File not recognized: {filename}')
-        if allfiles_ok == 3:       
+                print(f"File not recognized: {filename}")
+        if allfiles_ok == 3:
             # Process CloudSat data
             dardar = dp.process_dardar(dardar)
             ice2c = dp.process_2cice(ice2c, cldclass)
-            
+
             # get first and last times for CloudSat granule as a datetime object
-            start, stop = dp.get_time_range(dardar['time'])
+            start, stop = dp.get_time_range(dardar["time"])
             # Generate local ccic time grid
             ccic_t_grid = dp.get_local_time_grid(start, stop, GRIDSAT_dt)
-            
-            # Load CCIC data 
+
+            # Load CCIC data
             try:
                 ccic_ds = dp.load_ccic_grid(ccic_t_grid, ccic_datadir)
             except Exception as e:
                 ccic_ds = None
-                print(f'ERROR loading ccic data: ', e)
+                print(f"ERROR loading ccic data: ", e)
 
             # get CloudSat coordinates
-            times = dardar['time']
-            lats = dardar['lat']
-            lons = dardar['lon']
+            times = dardar["time"]
+            lats = dardar["lat"]
+            lons = dardar["lon"]
+            coords = xr.Dataset(
+                {
+                    "time": (("rays"), times),
+                    "lons": (("rays"), lons),
+                    "lats": (("rays"), lats),
+                }
+            )
             if ccic_ds is not None:
                 # Interpolate CCIC data
-                result = dp.interpolate_data(
-                    ccic_ds,
-                    times,
-                    lats, 
-                    lons,
-                    list(ccic_ds.keys())
-                    #batch_size=100,
+                result = ccic_ds.interp(
+                    time=coords.time, latitude=coords.lats, longitude=coords.lons
                 )
             else:
                 # set values to nan
-                result = {
-                    'tiwp'           :   np.empty(lats.shape)*np.nan,
-                    'cloud_prob_2d'  :   np.empty(lats.shape)*np.nan,
-                }
+                result = xr.Dataset(
+                    {
+                        "tiwp": (("rays",), np.empty(lats.shape) * np.nan),
+                        "cloud_prob_2d": (("rays",), np.empty(lats.shape) * np.nan),
+                    }
+                )
             # Create dataset to save
             ds = xr.Dataset(
                 data_vars=dict(
-                    iwp_ccic=(['rays'], result['tiwp']),
-                    iwp_2cice=(['rays'], ice2c['iwp']),
-                    iwp_dardar=(['rays'], dardar['iwp']),
-                    cloud_mask_ccic=(['rays'], result['cloud_prob_2d']),
-                    cloud_mask_2cice=(['rays'], ice2c['cm']),
-                    cloud_mask_dardar=(['rays'], dardar['cm']),
+                    iwp_ccic=(["rays"], result["tiwp"].data),
+                    iwp_2cice=(["rays"], ice2c["iwp"]),
+                    iwp_dardar=(["rays"], dardar["iwp"]),
+                    cloud_mask_ccic=(["rays"], result["cloud_prob_2d"].data),
+                    cloud_mask_2cice=(["rays"], ice2c["cm"]),
+                    cloud_mask_dardar=(["rays"], dardar["cm"]),
                 ),
                 coords=dict(
                     rays=np.arange(lons.size),
-                    longitude=(['rays'], lons),
-                    latitude=(['rays'], lats),
-                    time=(['rays'], times),
+                    longitude=(["rays"], lons),
+                    latitude=(["rays"], lats),
+                    time=(["rays"], times),
                 ),
                 attrs=dict(
                     cloudsat_granule=id_,
@@ -116,63 +156,75 @@ def process_granule(id_, granules, paths):
                 ),
             )
             # Name of output file
-            outfile = f'ccicgridsat_dardar_2cice_{tstamp}_{id_}.nc'
+            outfile = f"ccicgridsat_dardar_2cice_{tstamp}_{id_}.nc"
             outpath = os.path.join(output_dir, outfile)
             ds.to_netcdf(outpath)
-            message = f'Processesing done for granule {id_}'
+            message = f"Processesing done for granule {id_}"
         else:
-            message = f'Maybe wierd filepaths for granule {id_}'
+            message = f"Maybe wierd filepaths for granule {id_}"
     else:
-        message = f'Incomplete data for granule {id_}'
+        message = f"Incomplete data for granule {id_}"
     return message
 
-def run(args):
-    id_ = args['id_']
-    granules = args['granules']
-    paths = args['paths']
+
+def run(args: Dict[str, Any]) -> str:
+    """
+    Wrapper function to process a single granule.
+
+    Args:
+        args: A dictionary containing the arguments to the process_granule function.
+
+    Return:
+        A string indicating the processing statue.
+    """
+    id_ = args["id_"]
+    granules = args["granules"]
+    paths = args["paths"]
     return process_granule(id_, granules, paths)
 
 
 if __name__ == "__main__":
 
     # Path to CCIC data
-    datadir_ccic = '/home/hallborn/mnt/sun/ccic/gridsat/'
+    datadir_ccic = "/home/spfrnd/sun/ccic/gridsat/"
     # Paths to CloudSat data
-    datadir_dardar = '/data/s6/L2_ext/DARDAR/dardar3.1/'
-    datadir_2cice = '/data/s6/L2_ext/2C-ICE/'
-    datadir_cldclass = '/data/s6/L2_ext/2B-CLDCLASS/'
+    datadir_dardar = "/data/s6/L2_ext/DARDAR/dardar3.1/"
+    datadir_2cice = "/data/s6/L2_ext/2C-ICE/"
+    datadir_cldclass = "/data/s6/L2_ext/2B-CLDCLASS/"
 
     # path to store output data
-    output_dir = '/scratch/ccic_climate_record/cloudsat_collocations'
+    output_dir = "/scratch/ccic_climate_record/data/cloudsat"
     os.makedirs(output_dir, exist_ok=True)
 
     paths = {
-        'output_dir'    : output_dir,
-        'ccic_datadir'  : datadir_ccic,
+        "output_dir": output_dir,
+        "ccic_datadir": datadir_ccic,
     }
 
     cloudsat_paths = {
-        'dardar'    :   datadir_dardar,
-        '2cice'     :   datadir_2cice,
-        'cldclass'  :   datadir_cldclass,
+        "dardar": datadir_dardar,
+        "2cice": datadir_2cice,
+        "cldclass": datadir_cldclass,
     }
 
-    years = [2017]
+    years = [2006]
     for yr in years:
 
-        for month in range(1, 2):
-            
+        for month in range(1, 12):
+
             # Get dict with existing granules
             granules = get_granules(yr, month, cloudsat_paths)
-            
-            arguments = [{'id_' : id_, 'granules' : granules, 'paths' : paths} for id_ in granules]
-            
+            arguments = [
+                {"id_": id_, "granules": granules, "paths": paths} for id_ in granules
+            ]
+
             # the number of CPU cores to use
-            nprocesses = 8 #multiprocessing.cpu_count() 
+            nprocesses = 8  # multiprocessing.cpu_count()
+            nprocesses = 1
             count = 1
             with multiprocessing.Pool(processes=nprocesses) as pool:
                 for id_message in pool.imap(run, arguments):
-                    print(f'{count}/{len(arguments)} {id_message}') 
+                    print(f"{count}/{len(arguments)} {id_message}")
                     count += 1
 
-            print(f'Finnished processing {yr}, month {str(month).zfill(2)}')
+            print(f"Finished processing {yr}, month {str(month).zfill(2)}")
